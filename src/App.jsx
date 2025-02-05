9ec0a64f93857a86d44da206b30000173856b232
filipkk78 <filipkk78@gmail.com>
2025-02-05 17:49:45 +0100

    Add screen width responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
-import "./App.css";
+import "./styles/App.css";
+import General from "./components/General";
+import Education from "./components/Education";
+import Experience from "./components/Experience";
+import GenerateCv from "./components/Cv";
 
 function App() {
   return (
@@ -7,8 +11,26 @@ function App() {
         <header>
           <h1>CV Generator App</h1>
         </header>
-        <aside></aside>
-        <main></main>
+        <aside>
+          <General></General>
+          <Education></Education>
+          <Experience></Experience>
+        </aside>
+        <main>
+          <GenerateCv
+            name="John Smith"
+            email="jsmith@gmail.com"
+            phone="123456789"
+            school="Boston University"
+            title="Bachelor"
+            studyDate="05-02-2022"
+            company="Google"
+            position="Junior Fullstack Developer"
+            responsibilities="Something something bla bla"
+            workedFrom="07-14-2022"
+            workedUntil="02-05-2025"
+          ></GenerateCv>
+        </main>
         <footer>
           <h2>filipkk78</h2>
         </footer>
