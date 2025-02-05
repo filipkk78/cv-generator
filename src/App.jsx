eab301d4e241688607517c5e7e5bc76554a1b493
filipkk78 <filipkk78@gmail.com>
2025-02-05 16:27:22 +0100

    Switch to rendering whole page with react

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import "./App.css";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="wrapper">
+        <header>
+          <h1>CV Generator App</h1>
+        </header>
+        <aside></aside>
+        <main></main>
+        <footer>
+          <h2>filipkk78</h2>
+        </footer>
+      </div>
+    </>
+  );
 }
 
 export default App;
